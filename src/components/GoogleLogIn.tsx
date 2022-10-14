f1aa104de238aae0daf0dc2e25cbad4a04138ba4
Freda Manu <fredamanu@Fredas-MacBook-Air.local>
2022-10-14 13:07:26 +0200

    resizing google btn on different screen

--- src/components/GoogleLogIn.tsx
+++ src/components/GoogleLogIn.tsx
@@ -6,32 +6,43 @@ import { GoogleLogin } from '@react-oauth/google'
 import { userLogin } from '../redux/actions/user'
 
 export default function GoogleLogIn() {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const googleAuthResponse = (response: any) => {
-    axios
-      .post('https://nc-store-api.herokuapp.com/auth/google', {
-        id_token: response.credential,
-      })
-      .then((response: any) => {
-        dispatch(userLogin(response.data))
-        navigate('/')
-      })
-      .catch((error: any) => {
-        console.log(error)
-        navigate('/signin')
-      })
-  }
+ const dispatch = useDispatch()
+ const navigate = useNavigate()
+ const googleAuthResponse = (response: any) => {
+  axios
+   .post('https://nc-store-api.herokuapp.com/auth/google', {
+    id_token: response.credential
+   })
+   .then((response: any) => {
+    dispatch(userLogin(response.data))
+    navigate('/')
+   })
+   .catch((error: any) => {
+    console.log(error)
+    navigate('/signin')
+   })
+ }
 
-  return (
-    <div className="login-btn-container">
-      <GoogleLogin
-        onSuccess={googleAuthResponse}
-        onError={() => {
-          console.log('Login Failed')
-        }}
-        width="100%"
-      />
-    </div>
-  )
+ return (
+  <div>
+   <div className="login-btn-container screen-lg" id="google">
+    <GoogleLogin
+     onSuccess={googleAuthResponse}
+     onError={() => {
+      console.log('Login Failed')
+     }}
+     width="350"
+    />
+   </div>
+   <div className="login-btn-container mobile" id="google2">
+    <GoogleLogin
+     onSuccess={googleAuthResponse}
+     onError={() => {
+      console.log('Login Failed')
+     }}
+     width="250"
+    />
+   </div>
+  </div>
+ )
 }
