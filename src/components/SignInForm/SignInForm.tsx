cd8fe74128f41b05501213de8912a5e344425df7
Freda Manu <fredamanu@Fredas-MacBook-Air.local>
2022-11-14 14:23:28 +0100

    using splice to keep cartItem in place when changing quantities

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -1,116 +1,142 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { Paper, Divider } from '@mui/material'
-import { toast } from 'react-hot-toast'
-import axios from 'axios'
-import { useForm } from 'react-hook-form'
-import Form from 'react-bootstrap/Form'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Paper, Divider } from "@mui/material";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import Form from "react-bootstrap/Form";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
-import GoogleLogIn from '../GoogleLogIn'
-import { userLogin } from '../../redux/actions/user'
-import './SignInForm.css'
+import GoogleLogIn from "../GoogleLogIn";
+import { userLogin } from "../../redux/actions/user";
+import "./SignInForm.css";
 
 type FormData = {
- email: string
- password: string
-}
+  email: string;
+  password: string;
+};
 
 const AuthForm = () => {
- const navigate = useNavigate()
- const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
 
- const {
-  register,
-  handleSubmit,
-  formState: { errors }
- } = useForm<FormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
- const onSubmit = handleSubmit(async (data) => {
-  await axios
-   .post('https://nc-store-api.herokuapp.com/api/v1/users/login', data)
-   .then((response) => {
-    dispatch(userLogin(response.data))
-    navigate('/')
-   })
-   .catch((error) => {
-    if (error.response.data.message === 'user does not exist') {
-     toast.error('No account is set up for this email. Please Signup')
-     navigate('/signup')
-    } else if (error.response.data.message === 'password is incorrect') {
-     toast.error('Wrong email or password. Please try again')
-    } else if (error.response.data.message === 'no password') {
-     toast.error('Please use forgot password to create a new one')
-    } else {
-     toast('Log in failed. Please try again later')
-    }
-   })
- })
+  const onSubmit = handleSubmit(async (data) => {
+    await axios
+      .post("https://nc-store-api.herokuapp.com/api/v1/users/login", data)
+      .then((response) => {
+        dispatch(userLogin(response.data));
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.response.data.message === "user does not exist") {
+          toast.error("No account is set up for this email. Please Signup");
+          navigate("/signup");
+        } else if (error.response.data.message === "password is incorrect") {
+          toast.error("Wrong email or password. Please try again");
+        } else if (error.response.data.message === "no password") {
+          toast.error("Please use forgot password to create a new one");
+        } else {
+          toast("Log in failed. Please try again later");
+        }
+      });
+  });
 
- return (
-  <Paper elevation={3} className="login-wrapper">
-   <div>
-    <p className="login-title">Sign in</p>
-    <Form onSubmit={onSubmit}>
-     <div>
-      <div className="login-fields-container">
-       <Form.Label>Email</Form.Label>
-       <Form.Control
-        className="login-inputs"
-        type="email"
-        // placeholder="Enter email"
-        {...register('email', {
-         required: 'Email Address is required',
-         pattern: {
-          value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-          message: 'You have entered an invalid email address!'
-         }
-        })}
-       />
-       <p>{errors.email?.message}</p>
+  return (
+    <Paper elevation={3} className="login-wrapper">
+      <div>
+        <p className="login-title">Sign in</p>
+        <Form onSubmit={onSubmit}>
+          <div>
+            <div className="login-fields-container">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                className="login-inputs"
+                type="email"
+                // placeholder="Enter email"
+                {...register("email", {
+                  required: "Email Address is required",
+                  pattern: {
+                    value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                    message: "You have entered an invalid email address!",
+                  },
+                })}
+              />
+              <p>{errors.email?.message}</p>
+            </div>
+            <div className="login-fields-container">
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  cursor: "pointer",
+                }}
+              >
+                <Form.Label>Password</Form.Label>
+                <div>
+                  {showPassword ? (
+                    <VisibilityOffIcon
+                      onClick={() => {
+                        setShowPassword(false);
+                      }}
+                    />
+                  ) : (
+                    <VisibilityIcon
+                      onClick={() => {
+                        setShowPassword(true);
+                      }}
+                    />
+                  )}
+                </div>
+              </div>
+              <Form.Control
+                className="login-inputs"
+                type={showPassword ? "text" : "password"}
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "Your password must be at least 8 characters",
+                  },
+                })}
+              />
+              <p>{errors.password?.message}</p>
+            </div>
+          </div>
+          <div className="login-btn-container">
+            <button className="login-btn" type="submit">
+              <input
+                aria-label="submit"
+                type="submit"
+                value="Continue"
+                className="submit"
+              />
+              Continue
+            </button>
+          </div>
+          <Divider>Or</Divider>
+          <GoogleLogIn />
+        </Form>
+        <div className="login-actions-container">
+          <a href="/signup">
+            <p className="p1">Don't have an account?</p>
+            <p className="p2">Sign up</p>
+          </a>
+          <a href="/forgot-password">
+            <p className="p3">Forgot your password</p>
+          </a>
+        </div>
       </div>
-      <div className="login-fields-container">
-       <Form.Label>Password</Form.Label>
-       <Form.Control
-        className="login-inputs"
-        type="password"
-        {...register('password', {
-         required: 'Password is required',
-         minLength: {
-          value: 8,
-          message: 'Your password must be at least 8 characters'
-         }
-        })}
-       />
-       <p>{errors.password?.message}</p>
-      </div>
-     </div>
-     <div className="login-btn-container">
-      <button className="login-btn" type="submit">
-       <input
-        aria-label="submit"
-        type="submit"
-        value="Continue"
-        className="submit"
-       />
-       Continue
-      </button>
-     </div>
-     <Divider>Or</Divider>
-     <GoogleLogIn />
-    </Form>
-    <div className="login-actions-container">
-     <a href="/signup">
-      <p className="p1">Don't have an account?</p>
-      <p className="p2">Sign up</p>
-     </a>
-     <a href="/forgot-password">
-      <p className="p3">Forgot your password</p>
-     </a>
-    </div>
-   </div>
-  </Paper>
- )
-}
+    </Paper>
+  );
+};
 
-export default AuthForm
+export default AuthForm;
--- src/redux/reducers/cart.tsx
+++ src/redux/reducers/cart.tsx
@@ -1,129 +1,206 @@
-import { ActionTypes, Cart, CartItem } from '../../types'
-import * as actions from '../actions/types'
-import { toast } from 'react-hot-toast'
+import { ActionTypes, Cart, CartItem } from "../../types";
+import * as actions from "../actions/types";
+import { toast } from "react-hot-toast";
 
 const initialState: Cart = {
   cartItems: [],
   totalPrice: 0,
   totalQuantities: 0,
   showCart: false,
-}
+};
 
 export const cart = (state = initialState, action: ActionTypes) => {
   switch (action.type) {
     case actions.ADD_TO_CART:
+      //check if items is already in the cart
       const isInCart = state.cartItems.some((item) => {
-        return item._id === action.payload._id
-      })
+        return item._id === action.payload._id;
+      });
+
+      // if its is already in the cart then this will be the updated cart items
+      //For this iterate throught the cartItems and update the item's qty
       const updatedCartItems = state.cartItems.map((cartItem) => {
         if (cartItem._id === action.payload._id)
           return {
             ...cartItem,
             qty: cartItem.qty + action.payload.qty,
-          }
+          };
         return {
           ...cartItem,
-        }
-      })
+        };
+      });
+
+      //Now return state depending on if isInCart is true or false
       if (isInCart) {
         toast.success(
           `${action.payload.qty} ${action.payload.name} added to cart`,
           {
-            icon: '👏',
-            position: "bottom-left"
+            icon: "👏",
+            position: "bottom-left",
           }
-        )
+        );
         return {
           ...state,
           totalPrice:
             state.totalPrice + action.payload.price * action.payload.qty,
           totalQuantities: state.totalQuantities + action.payload.qty,
           cartItems: updatedCartItems,
-        }
+        };
       }
       toast.success(
         `${action.payload.qty} ${action.payload.name} added to cart`,
         {
-          icon: '👏',
-          position: "bottom-left"
+          icon: "👏",
+          position: "bottom-left",
         }
-      )
+      );
       return {
         ...state,
         totalPrice:
           state.totalPrice + action.payload.price * action.payload.qty,
         totalQuantities: state.totalQuantities + action.payload.qty,
         cartItems: [action.payload, ...state.cartItems],
-      }
+      };
 
+    //Setting the state of show cart
     case actions.OPEN_CART:
       return {
         ...state,
         showCart: true,
-      }
+      };
 
     case actions.CLOSE_CART:
       return {
         ...state,
         showCart: false,
-      }
+      };
 
+    //Setting the state of cart item quantity increase
     case actions.INCREASE_CART_ITEM_QUANTITY:
+      //First find the item whose quanity you want to increase in the cartItems array
       const foundProduct = state.cartItems.find(
         (item) => item._id === action.payload
-      ) as CartItem
+      ) as CartItem;
 
-      const newCart = state.cartItems.filter(
-        (item) => item._id !== action.payload
-      )
+      // Now find the index of that item
+      const indexOfFoundProduct = state.cartItems.indexOf(foundProduct);
+
+      // Now update the quantity of the found item assigning it to a new variable
+      let newProduct = { ...foundProduct, qty: foundProduct.qty + 1 };
+
+      //replace the found item inside the cartItems array with the updated version
+      state.cartItems.splice(indexOfFoundProduct, 1, newProduct);
 
       return {
         ...state,
-        cartItems: [{ ...foundProduct, qty: foundProduct.qty + 1 }, ...newCart],
-
+        cartItems: [...state.cartItems],
         totalPrice: state.totalPrice + foundProduct.price,
         totalQuantities: state.totalQuantities + 1,
-      }
+      };
+
+    //Setting the state for reducing cart item quanity
     case actions.DECREASE_CART_ITEM_QUANTITY:
+      //First find the item to be decrease with the cartItem array
       const foundProductToDecrease = state.cartItems.find(
         (item) => item._id === action.payload
-      ) as CartItem
+      ) as CartItem;
 
-      const newCart2 = state.cartItems.filter(
-        (item) => item._id !== action.payload
-      )
+      //Now find the index of the found item
+      const indexOfFoundProductToDecrease = state.cartItems.indexOf(
+        foundProductToDecrease
+      );
+
+      //Now update the found item and assign it to a new variable
+      let newProductToDecrease = {
+        ...foundProductToDecrease,
+        qty: foundProductToDecrease.qty - 1,
+      };
+
+      //Now replace the found product with the updated one
+      state.cartItems.splice(
+        indexOfFoundProductToDecrease,
+        1,
+        newProductToDecrease
+      );
+
+      //Then check if the quanity of foundProduct is less than or greater than 1
+      //If greater than one just return state with cartItems as a copy of the cartItems
       if (foundProductToDecrease.qty > 1) {
         return {
           ...state,
           cartItems: [
-            { ...foundProductToDecrease, qty: foundProductToDecrease.qty - 1 },
-            ...newCart2,
+            // { ...foundProductToDecrease, qty: foundProductToDecrease.qty - 1 },
+            // ...newCart2,
+            ...state.cartItems,
           ],
 
           totalPrice: state.totalPrice - foundProductToDecrease.price,
           totalQuantities: state.totalQuantities - 1,
-        }
+        };
       }
-      return {
-        ...state,
-        cartItems: [{ ...foundProductToDecrease, qty: 1 }, ...newCart2],
+      //If qty is less than 1, confirm from user is they want to remove the item from the cart with window.confirm
+      if (
+        window.confirm(
+          "Are you sure you want to remove this item from your cart?"
+        )
+      ) {
+        toast.success(
+          `${newProductToDecrease.qty} ${newProductToDecrease.name} removed from cart`,
+          {
+            icon: "😔",
+            position: "bottom-left",
+          }
+        );
+        //If positive response from the item from the array and adjust total price and total quanities
+        state.cartItems.splice(indexOfFoundProductToDecrease, 1);
+        return {
+          ...state,
+          cartItems: [...state.cartItems],
+          totalPrice:
+            state.totalPrice -
+            foundProductToDecrease.price * foundProductToDecrease.qty,
+          totalQuantities: state.totalQuantities - foundProductToDecrease.qty,
+        };
+      }
+      //if negative response, reset the quanity of the found item to 1 and return a new state
+      else {
+        newProductToDecrease = {
+          ...foundProductToDecrease,
+          qty: 1,
+        };
+        state.cartItems.splice(
+          indexOfFoundProductToDecrease,
+          1,
+          newProductToDecrease
+        );
+        toast.success(
+          `${newProductToDecrease.qty} ${newProductToDecrease.name} was not removed from cart`,
+          {
+            icon: "🤝",
+            position: "bottom-left",
+          }
+        );
+        return {
+          ...state,
+          carItems: [...state.cartItems],
+        };
       }
 
     case actions.REMOVE_FROM_CART:
       const foundProductToRemove = state.cartItems.find(
         (item) => item._id === action.payload
-      ) as CartItem
+      ) as CartItem;
 
       const newCartItems3 = state.cartItems.filter(
         (item) => item._id !== action.payload
-      )
+      );
       toast.success(
         `${foundProductToRemove.qty} ${foundProductToRemove.name} removed from cart`,
         {
-          icon: '😔',
-          position: "bottom-left"
+          icon: "😔",
+          position: "bottom-left",
         }
-      )
+      );
       return {
         ...state,
         cartItems: [...newCartItems3],
@@ -131,7 +208,7 @@ export const cart = (state = initialState, action: ActionTypes) => {
           state.totalPrice -
           foundProductToRemove.price * foundProductToRemove.qty,
         totalQuantities: state.totalQuantities - foundProductToRemove.qty,
-      }
+      };
 
     case actions.CART_RESET:
       return {
@@ -140,9 +217,9 @@ export const cart = (state = initialState, action: ActionTypes) => {
         totalPrice: 0,
         totalQuantities: 0,
         showCart: false,
-      }
+      };
 
     default:
-      return state
+      return state;
   }
-}
+};
